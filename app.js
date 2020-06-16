6f19e6f05726c913f58de5a2467cfd248ce311e6
Diego97-webdev <shizusenpai97@gmail.com>
2020-06-16 19:46:24 -0300

    añadido el menu desplegable

--- app.js
+++ app.js
@@ -15,3 +15,47 @@ var swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//MENU DESPLEGABLE
+
+const hamburguesa = document.querySelector(".menu-movil");
+const menuDesplegable = document.querySelector(".menu-desplegable");
+const links = document.querySelectorAll(".links-desplegable");
+const botonCerrar = document.querySelector(".contenedor-btn-cerrar");
+const botonBuscarDesplegable = document.querySelector(".btn-desplegable");
+const inputDesplegable = document.querySelector(".input-buscar-desplegable");
+
+const cruzUno = document.querySelector(".cruz-uno");
+const cruzDos = document.querySelector(".cruz-dos");
+
+const cruces = document.querySelectorAll(".cruz");
+
+hamburguesa.addEventListener("click", mostrarDesplegable);
+
+function mostrarDesplegable() {
+  menuDesplegable.style.left = "0";
+  if ((cruces.item(0).style.transform = "rotate(0)")) {
+    cruces.item(0).style.transform = "rotate(135deg)";
+    cruces.item(1).style.transform = "rotate(-135deg)";
+    //PARA RESETEAR LOS VALORES POR SI EL MENU SE ABRE MAS DE UNA VEZ
+  }
+}
+
+botonCerrar.addEventListener("click", cerrarDesplegable);
+
+function cerrarDesplegable() {
+  menuDesplegable.style.left = "-100%";
+  cruces.forEach((cruz) => {
+    cruz.style.transform = "rotate(0)";
+  });
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", cerrarDesplegable);
+});
+
+botonBuscarDesplegable.addEventListener("click", borrarContenido);
+
+function borrarContenido() {
+  inputDesplegable.value = "";
+}
